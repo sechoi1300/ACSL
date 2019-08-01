c7900118f5466a9c23176164c5f9d23294d2ae04
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-08-01 14:08:05 -0700

    Update C1JR_Scrabble.java

--- _2013_2014/src/C1JR_Scrabble.java
+++ _2013_2014/src/C1JR_Scrabble.java
@@ -14,65 +14,60 @@ public class C1JR_Scrabble {
 			for(int gLoop = 0; gLoop < 2; gLoop++) {
 				
 				String[] word = in.nextLine().split(" ");
-				int[] location = new int[4];
+				int[] templocation = new int[5];
 				int[] point = new int[4];
 				int totalPoint = 0;
 				
 				for(int loop = 0; loop < 5; loop++) {
 					
-					location[0] = in.nextInt();
+				int location = Integer.parseInt(in.nextLine());
 				
-					for(int i = 1; i < 4; i++) {
-						location[i] = location[0] + i;
-					}
-					
-					for(int i = 0; i < 4; i++) {
-						if(word[i].equals("A") || word[i].equals("E")) {
-							point[i] = 1;
-						} else if(word[i].equals("D") || word[i].equals("R")) {
-							point[i] = 2;
-						} else if(word[i].equals("B") || word[i].equals("M")) {
-							point[i] = 3;
-						} else if(word[i].equals("V") || word[i].equals("Y")) {
-							point[i] = 4;
-						} else {
-							point[i] = 8;
-						}
-					}
-					
-					for(int i = 0; i < 4; i++) {
-						if(location[i] % 6 != 0 && location[i] % 3 == 0) {
-							point[i] *= 2;
-						} else if(location[i] % 5 == 0) {
-							point[i] *= 5;
-						} else if(location[i] % 7 == 0) {
-							doubleWord = true;
-						} else if(location[i] % 8 == 0) {
-							tripleWord = true;
-						} else {
-							doubleWord = false;
-							tripleWord = false;
-						}
-					}
-					
-					for(int i = 0; i < 4; i++) {
-						totalPoint += point[i];
-					}
-					
-					if(doubleWord = true) {
-						totalPoint *= 2;
-					} else if(tripleWord = true) {
-						totalPoint *= 3;
-					} else {
-						continue;
-					}
-					
-					System.out.println(totalPoint);
-					totalPoint = 0;
-					doubleWord = false;
-					tripleWord = false;
+				for(int i = 0; i < 4; i++) {
+					templocation[i] = location + i;
 				}
+					
+				for(int i = 0; i < 4; i++) {
+					if(word[i].equals("A") || word[i].equals("E")) {
+						point[i] = 1;
+					} else if(word[i].equals("D") || word[i].equals("R")) {
+						point[i] = 2;
+					} else if(word[i].equals("B") || word[i].equals("M")) {
+						point[i] = 3;
+					} else if(word[i].equals("V") || word[i].equals("Y")) {
+						point[i] = 4;
+					} else {
+						point[i] = 8;
+					}
+				}
+					
+				for(int i = 0; i < 4; i++) {
+					if(templocation[i] % 3 == 0 && templocation[i] % 6 != 0) {
+						point[i] *= 2;
+					} else if(templocation[i] % 5 == 0) {
+						point[i] *= 3;
+					} else if(templocation[i] % 7 == 0) {
+						doubleWord = true;
+					} else if(templocation[i] % 8 == 0) {
+						tripleWord = true;
+					}
+				}
+					
+				for(int i = 0; i < 4; i++) {
+					totalPoint += point[i];
+				}
+					
+				if(doubleWord) {
+					totalPoint *= 2;
+				} else if(tripleWord) {
+					totalPoint *= 3;
+				}
+					
+				System.out.println(totalPoint);
+				totalPoint = 0;
+				doubleWord = false;
+				tripleWord = false;
 				
+				}
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
