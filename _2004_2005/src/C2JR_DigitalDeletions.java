ad746e6259ca03507962c4b7cb28d08db2da04c2
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-08-08 14:15:38 -0700

    .

---
+++ _2004_2005/src/C2JR_DigitalDeletions.java
@@ -0,0 +1,9 @@
+
+public class C2JR_DigitalDeletions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- _2016_2017/src/C1JR_AGRAM.java
+++ _2016_2017/src/C1JR_AGRAM.java
@@ -3,12 +3,6 @@ import java.io.*;
 
 public class C1JR_AGRAM {
 	
-	static boolean dealerCard1 = false;
-	static boolean dealerCard2 = false;
-	static boolean dealerCard3 = false;
-	static boolean dealerCard4 = false;
-	static boolean dealerCard5 = false;
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
@@ -17,26 +11,79 @@ public class C1JR_AGRAM {
 			for(int gLoop = 0; gLoop < 10; gLoop++) {
 				int opponentCardNum = in.nextInt();
 				String opponentCardSuit = in.next();
-				int dealerCard1Num = in.nextInt();
-				String dealerCard1Suit = in.next();
-				int dealerCard2Num = in.nextInt();
-				String dealerCard2Suit = in.next();
-				int dealerCard3Num = in.nextInt();
-				String dealerCard3Suit = in.next();
-				int dealerCard4Num = in.nextInt();
-				String dealerCard4Suit = in.next();
-				int dealerCard5Num = in.nextInt();
-				String dealerCard5Suit = in.next();
 				
-				if(opponentCardSuit.equals("H")) {
-					
-				} else if(opponentCardSuit.equals("D")) {
-					
-				} else if(opponentCard)
+				int[] dealerCardNum = new int[5];
+				String[] dealerCardSuit = new String[5];
+				int[] dealerCard = new int[5];
+				
+				for(int i = 0; i < 5; i++) {
+					dealerCardNum[i] = in.nextInt();
+					dealerCardSuit[i] = in.next();
+				}
+				
+				if ( !higher2(opponentCardNum, opponentCardSuit, dealerCardNum, dealerCardSuit) ) {
+					lowest(opponentCardNum, opponentCardSuit, dealerCardNum, dealerCardSuit, dealerCard);
+				}
+				
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static boolean higher(int opponentCardNum, String opponentCardSuit, int[] dealerCardNum, String[] dealerCardSuit, int[] dealerCard) {
+		for(int i = 0; i < 5; i++) {
+			if(opponentCardSuit.equals(dealerCardSuit[i])) {
+				dealerCard[i] = dealerCardNum[i];
+				dealerCardSuit[0] = dealerCardSuit[i];
+			}
+		}
+		
+		for(int l = 0; l < dealerCard.length; l++) {
+			if(dealerCard[l] > opponentCardNum) {
+				dealerCard[0] = dealerCard[l];
+				return true;
+			} else {
+				continue;
+			}
+		}
+		
+		System.out.println(dealerCard[0] + " " + dealerCardSuit[0]);
+		return false;
+	}
 
+	public static boolean higher2(int opponentCardNum, String opponentCardSuit, int[] dealerCardNum, String[] dealerCardSuit) {
+		int dealerCard = 0;
+		for(int i = 0; i < dealerCardNum.length; i++) {
+			if(opponentCardSuit.equals(dealerCardSuit[i]) && opponentCardNum < dealerCardNum[i]) {
+				if ( dealerCard == 0 || dealerCardNum[i] < dealerCard ) {
+					dealerCard = dealerCardNum[i];
+				}
+			}
+		}
+
+		if ( opponentCardNum < dealerCard ) {
+			System.out.println(dealerCard + " " + opponentCardSuit);
+			return true;
+		}
+
+		return false;
+	}
+	
+	public static boolean lowest(int opponentCardNum, String opponentCardSuit, int[] dealerCardNum, String[] dealerCardSuit, int[] dealerCard) {
+		int min = 0;
+		for(int i = 0; i < 5; i++) {
+			if(opponentCardSuit.equals(dealerCardSuit[i])) {
+				dealerCard[i] = dealerCardNum[i];
+				dealerCardSuit[0] = dealerCardSuit[i];
+			}
+		}
+		
+		for(int l = 0; l < dealerCard.length; l++) {
+			min = Math.min(min, dealerCard[l]);
+		}
+		System.out.println(min + " " + dealerCardSuit[0]);
+		return true;
+	}
+	
 }
