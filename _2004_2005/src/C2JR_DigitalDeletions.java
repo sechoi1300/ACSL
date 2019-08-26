d5b911875f492c4cc4cb03a548333930cc243597
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2019-08-25 22:45:06 -0700

    .

--- _2004_2005/src/C2JR_DigitalDeletions.java
+++ _2004_2005/src/C2JR_DigitalDeletions.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class C2JR_DigitalDeletions {
 
@@ -9,40 +10,51 @@ public class C2JR_DigitalDeletions {
 			Scanner in = new Scanner(new File("0405c2jr.in"));
 			
 			for(int gLoop = 0; gLoop < 7; gLoop++) {
-				String[] num = in.nextLine().split(" ");
+				int num = in.nextInt();
+				String[] numbers = in.nextLine().split(" ");
 				
 				ArrayList<Integer> list = new ArrayList<Integer>();
 				int moves = 0;
 				
+				for(int i = 0; i < num; i++) {
+					list.add(Integer.parseInt(numbers[i]));
+				}
 				
-				for(int i = 0; i < num.length; i++) {
-					list.add(Integer.parseInt(num[i]));
+				System.out.println(num);
+				System.out.println(Arrays.toString(numbers));
+				for(int i = 0; i < num; i++) {
+					System.out.println(list.get(i));
+				}
+				
+				/*
+				for(int i = list.size() - 1; i > 0; i--) {
+					if(list.get(i) == 0) {
+						list.subList(0, i).clear();
+						moves++;
+					}
 				}
 				
 				while(list.size() > 0) {
-					for(int i = list.size() - 1; i > 0; i--) {
-						if(list.get(i) == 0) {
-							for(int l = 0; l < i; l++) {
-								list.remove(l);
-							}
-							moves++;
-						}
-					}
 					
 					int max = 0;
+					int x = 0;
 					for(int i = list.size() - 1; i > 0; i--) {
 						max = Math.max(max, list.get(i));
+						x = list.indexOf(max);
 					}
 					if(max % 2 == 0) {
 						max -= 2;
+						list.set(x, max);
 						moves++;
 					} else {
 						max -= 1;
+						list.set(x, max);
 						moves++;
 					}
 				}
 				
 				System.out.println(moves);
+				*/
 				
 			}
 			
