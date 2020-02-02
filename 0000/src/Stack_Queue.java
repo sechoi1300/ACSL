b31f487a6426ebc25eb5833c5256b46c1c297c63
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2020-02-01 17:31:55 -0800

    .

---
+++ 0000/src/Stack_Queue.java
@@ -0,0 +1,9 @@
+
+public class Stack_Queue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- _2019_2020/src/C2IN_ACSLSamenessFactor.java
+++ _2019_2020/src/C2IN_ACSLSamenessFactor.java
@@ -14,14 +14,13 @@ public class C2IN_ACSLSamenessFactor {
 	static String s1, s2, s1temp, s2temp;
 	static char[] char1, char2;
 	static int result;
-	static boolean repeat;
 	
 	public static void main(String[]args) throws IOException {
 		
 		in = new Scanner(new File("1920c2in.in"));
 		//in = new Scanner(new File("bleh"));
 		
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < 10; i++) {
 			
 			try {
 				init();
@@ -43,79 +42,54 @@ public class C2IN_ACSLSamenessFactor {
 		char1 = s1.toCharArray();
 		char2 = s2.toCharArray();
 		result = 0;
-		repeat = true;
 	}
 	
 	static void solve() {
-		while(repeat) {
+		while(true) {
 			for(int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
 				if(s1.charAt(i) == s2.charAt(i)) {
 					char1[i] = '-';
 					char2[i] = '-';
 				}
 			}
-			s1 = "";
-			s2 = "";
-			for(int i = 0; i < char1.length; i++) {
-				if(char1[i] == '-') {
-					continue;
-				} else {
-					s1 += char1[i];
-				}
-			}
-			for(int i = 0; i < char2.length; i++) {
-				if(char2[i] == '-') {
-					continue;
-				} else {
-					s2 += char2[i];
-				}
-			}
-			char1 = s1.toCharArray();
-			char2 = s2.toCharArray();
-			for(int i = 1; i < Math.min(s1.length(), s2.length()) - 1; i++) {
-				if(char1[i] == char2[i - 1]) {
-					char1[i - 1] = '-';
-				}
-				if(char2[i] == char1[i - 1]) {
-					char2[i - 1] = '-';
+			
+			convert();
+			
+			boolean a = false;
+			
+			for(int i = 0; i < Math.min(s1.length(), s2.length()) - 1; i++) {
+				if(char1[i] == char2[i + 1]) {
+					char2[i] = '-';
+					a = true;
+					break;
 				}
 				if(char2[i] == char1[i + 1]) {
 					char1[i] = '-';
-				}
-				if(char1[i] == char2[i + 1]) {
-					char2[i] = '-';
-				}
-			}
-			s1 = "";
-			s2 = "";
-			for(int i = 0; i < char1.length; i++) {
-				if(char1[i] == '-') {
-					continue;
-				} else {
-					s1 += char1[i];
+					a = true;
+					break;
 				}
 			}
-			for(int i = 0; i < char2.length; i++) {
-				if(char2[i] == '-') {
-					continue;
-				} else {
-					s2 += char2[i];
-				}
-			}
-			char1 = s1.toCharArray();
-			char2 = s2.toCharArray();
 			
-			for(int i = 0; i < char1.length; i++) {
-				if(s2.indexOf(char1[i]) == -1 && i == char1.length - 1) {
-					repeat = false;
+			convert();
+			
+			if(s1.length() - s2.length() == 1) {
+				if(char1[s1.length() - 1] == char2[s2.length() - 1]) {
+					char1[s1.length() - 2] = '-';
 				}
 			}
-			if(!repeat) break;
+			
+			convert();
+			
+			if(s2.length() - s1.length() == 1) {
+				if(char2[s2.length() - 1] == char1[s1.length() - 1]) {
+					char2[s2.length() - 2] = '-';
+				}
+			}
+			
+			convert();
+			
+			if(!a) break;
 		}
-		
-		System.out.println(Arrays.toString(char1));
-		System.out.println(Arrays.toString(char2));
-		
 		for(int i = 0; i < Math.min(char1.length, char2.length); i++) {
 			result += (char1[i] - char2[i]);
 		}
@@ -123,5 +97,27 @@ public class C2IN_ACSLSamenessFactor {
 		System.out.println(result);
 		
 	}
+	
+	static void convert() {
+		s1 = "";
+		s2 = "";
+		for(int i = 0; i < char1.length; i++) {
+			if(char1[i] == '-') {
+				continue;
+			} else {
+				s1 += char1[i];
+			}
+		}
+		for(int i = 0; i < char2.length; i++) {
+			if(char2[i] == '-') {
+				continue;
+			} else {
+				s2 += char2[i];
+			}
+		}
+		char1 = s1.toCharArray();
+		char2 = s2.toCharArray();
+	}
+	
 	
 }
